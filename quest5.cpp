c583e200a3d6884ae8ad8292a6f5ef5dd9be0390
coisasocuriosoporcoisas-cmd <coisasocuriosoporcoisas@gmail.com>
2025-12-03 19:57:08 -0300

    Create quest5.cpp

---
+++ quest5.cpp
@@ -0,0 +1,24 @@
+/*- Usando o vetor do exercício anterior, calcule e exiba:
+    A soma total dos números
+    A média aritmética dos valores
+*/
+
+#include <iostream>
+ using namespace std;
+int main(){
+	
+	int numeros[5];
+  int soma=0;
+	
+	for(int i=0; i<5; i++){
+		cout<<"informe numeros :"<<endl;
+		cin >>numeros[i];	
+		
+	}
+for(int i=0;i<5;i++){
+  soma+=numeros[i];
+  cout<<"soma atual"<<soma<<endl;
+
+}
+	return 0;
+}
