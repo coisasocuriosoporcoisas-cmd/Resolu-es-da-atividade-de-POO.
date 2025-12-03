08dcf19e4c6073724dddc92dd702a70b24c08937
coisasocuriosoporcoisas-cmd <coisasocuriosoporcoisas@gmail.com>
2025-12-03 20:38:28 -0300

    Create quest6_com_eeros.cpp

---
+++ quest6_com_eeros.cpp
@@ -0,0 +1,80 @@
+/*- Leia 8 números inteiros para dentro de um vetor e determine:
+    O maior valor e em qual posição ele está
+    O menor valor e em qual posição ele está
+*/
+
+
+#include <iostream>
+
+
+
+
+/*- Leia 8 números inteiros para dentro de um vetor e determine:
+    O maior valor e em qual posição ele está
+    O menor valor e em qual posição ele está
+*/
+
+ using namespace std;
+int main(){
+	
+	int numeros[5];
+	
+	//int maior = numeros[0];//
+    //int menor = numeros[0];//<-declara as variaveis aqui só quando o meu array já estive com seus valores definidos.
+	
+	
+	 for (int i = 0; i < 5; i++) {
+        cout << "Digite o valor para a posicao " << i+1 << ": ";
+        cin >> numeros[i];
+    }
+	
+	int maior = numeros[0];//elas estão aqui para evitar erros lógicos, pois se inicializamos elas antes de passamos os respectivais valores, ocorrera erros.
+    int menor = numeros[0];//
+	
+	for(int i=0;i<5;i++){
+		
+		
+     if (numeros[i]> maior ){
+		maior = numeros[i];//[i] para atribuir o maior valor a cada rodada, é compara caso não seja é menor, se não é maior, até finaliza a lista e as posições serem preechidas., ele compara com o anteriror.
+			  		}/*
+					  
+		for(int i=0;i<5;i++){
+			lista do array{1,2,7,9,0,8};
+			
+			
+			if (numeros[i]< maior(0 até agora.){
+				
+				maior= numero[i];
+				maior =1
+				
+				
+				
+
+			}
+           else
+	  	  	  {
+		   		menor = numeros[i];
+				}
+						
+		}			  
+					  
+					  */
+	 else
+	 {
+		menor = numeros[i];//
+	 }
+		
+	
+	}
+	
+	
+	
+	cout<<"maior:"<< maior <<endl;//
+    cout<<"menor:"<<menor<<endl;//
+	
+	return 0;
+}
+
+
+
+
